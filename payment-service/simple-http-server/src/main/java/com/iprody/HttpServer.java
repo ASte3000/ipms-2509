16da62ed427a94736b648b25a6ac86666aac48e1
ASte3000 <vatspb1@gmail.com>
2025-09-14 01:02:54 +0300

    HW01(HttpServer) - corrections by review

--- payment-service/simple-http-server/src/main/java/com/iprody/HttpServer.java
+++ payment-service/simple-http-server/src/main/java/com/iprody/HttpServer.java
@@ -7,120 +7,68 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.Date;
 
+/**
+ * Simple HTTP server - reads HTTP request and returns requested file (if found)
+ */
 public class HttpServer {
     private static final int PORT = 8080;
+    private static final String HTTP_METHOD_GET = "GET";
+    private static final String FILES_FOLDER = "simple-http-server/static/";
 
     public static void main(String[] args) throws IOException {
-        ServerSocket serverSocket = new ServerSocket(PORT);
-        System.out.printf("Server started at http://localhost:%s%n", PORT);
+        try (ServerSocket serverSocket = new ServerSocket(PORT)) {
+            System.out.printf("Server started at http://localhost:%s%n", PORT);
 
-        while (true) {
-            Socket socket = serverSocket.accept();
-            String systemTime = new Date().toString();
-            System.out.println("New client connected: " + systemTime);
+            while (true) {
+                try (Socket socket = serverSocket.accept()) {
+                    System.out.println("New client connected: " + new Date());
 
-            var reader = new BufferedReader(
-                    new InputStreamReader(socket.getInputStream(), StandardCharsets.UTF_8));
+                    var reader = new BufferedReader(
+                            new InputStreamReader(socket.getInputStream(), StandardCharsets.UTF_8));
 
-            while (!reader.ready());
+                    while (!reader.ready());    //waiting for request
 
-            var inputLinesList = new ArrayList<String>();
-            while (reader.ready()) {
-                var line = reader.readLine();
-                System.out.println(line);
-                inputLinesList.add(line);
-            }
+                    var responseWriter = new HttpResponsePrintWriter(socket.getOutputStream());
 
-            var mainParams = inputLinesList.isEmpty() ? null : inputLinesList.getFirst().split(" ");
-            if (mainParams != null && mainParams.length == HttpRequestParams.values().length
-                    && mainParams[HttpRequestParams.METHOD_NAME.ordinal()].equals("GET"))
-            {
-                var fileName = mainParams[HttpRequestParams.PATH.ordinal()];
-                getFile(fileName, socket.getOutputStream());
-            }
-
-            socket.close();
-        }
-
-    }
-
-    private static void getFile(String fileName, OutputStream out) throws IOException {
-        System.out.println("getFile: " + fileName);
-        var path = Path.of(fileName);
-        var cleanFileName = path.getFileName();
-        if (!path.equals(Path.of("/" + cleanFileName))) {
-            writeNotFound(out);
-        } else {
-            String sourcePathString = "simple-http-server/static/" + cleanFileName;
-            if (Files.exists(Paths.get(sourcePathString))) {
-                var outputStreamWriter = new OutputStreamWriter(out);
-                var writer = new PrintWriter(outputStreamWriter);
-
-
-                writer.println("HTTP/1.1 200 OK");
-
-                var extension = getFileExtension(sourcePathString);
-                writer.printf("Content-Type: %s", extension.getContentType());
-                writer.println();
-
-                FileReader fileReader = new FileReader(sourcePathString);
-                BufferedReader bufferedReader = new BufferedReader(fileReader);
-                var fileLines = bufferedReader.lines().toList();
-
-                var responseLength = 0L;
-                for (String line : fileLines) {
-                    responseLength += line.length();
+                    var fileName = getFileNameFromRequestHeader(reader.readLine());
+                    if (fileName != null) {
+                        processFileRequest(fileName, responseWriter);
+                    } else {
+                        responseWriter.writeStatus(HttpResponsePrintWriter.ERROR_400_BAD_REQUEST);
+                    }
                 }
-                writer.println("Content-Length: " + responseLength);
-                writer.println();
-
-                fileLines.forEach(writer::write);
-
-                writer.flush();
-            } else {
-                writeNotFound(out);
             }
         }
     }
 
-    private static void writeNotFound(OutputStream out) {
-        var writer = new PrintWriter(
-                new OutputStreamWriter(out));
-        writer.println("HTTP/1.1 404 NOT FOUND");
-        writer.flush();
-    }
-
-    private enum HttpRequestParams {
-        METHOD_NAME,
-        PATH,
-        PROTOCOL
-    }
-
-    private static FileExtension getFileExtension(String sourcePathString) {
-        var upperPath = sourcePathString.toUpperCase();
-        for (FileExtension extension : FileExtension.values()) {
-            if (upperPath.endsWith(extension.name()))
-                return extension;
+    private static String getFileNameFromRequestHeader(String headerFirstLine) {
+        var mainParams = headerFirstLine.split(" ");
+        if (mainParams.length == HttpRequestParams.values().length
+                && mainParams[HttpRequestParams.METHOD_NAME.ordinal()].equals(HTTP_METHOD_GET))
+        {
+            return mainParams[HttpRequestParams.PATH.ordinal()];
+        } else {
+            return null;
         }
-
-        return FileExtension.HTML;
     }
 
-    private enum FileExtension {
-        HTML("text/html; charset=UTF-8"),
-        JPEG("image/jpeg");
+    private static void processFileRequest(String fileName, HttpResponsePrintWriter responseWriter) throws IOException {
+        System.out.println("processFileRequest: " + fileName);
 
-        private final String contentType;
-
-        FileExtension(String contentType) {
-            this.contentType = contentType;
-        }
-
-        public String getContentType() {
-            return contentType;
+        var path = Path.of(fileName);
+        if (!path.getRoot().equals(path.getParent())) {
+            responseWriter.writeStatus(HttpResponsePrintWriter.ERROR_404_NOT_FOUND);    //requested file is not in the root folder
+        } else {
+            String sourcePathString = FILES_FOLDER + path.getFileName();
+            var sourcePath = Paths.get(sourcePathString);
+            if (Files.exists(sourcePath)) {
+                responseWriter.writeSuccessfulResponse(FileExtension.find(sourcePathString).getContentType(),
+                        Files.size(sourcePath), sourcePathString);
+            } else {
+                responseWriter.writeStatus(HttpResponsePrintWriter.ERROR_404_NOT_FOUND);
+            }
         }
     }
 }
